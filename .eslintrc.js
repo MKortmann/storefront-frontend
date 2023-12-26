48718b5e4066ea39bd8b12e677ca67202c7ecaee
MKortmann <companymvms@gmail.com>
2023-12-26 20:57:26 +0100

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    // Add any additional rules or overrides here
+  },
+}
